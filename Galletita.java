b413af946735653a01e9b4ea800fc3cd739a5fc9
alexiuna <alexiafacultad@gmail.com>
2020-09-24 14:26:11 -0300

    segundo commit: agregamos 2 archivos

---
+++ Galletita.java
@@ -0,0 +1,19 @@
+package proyecto1TDPparte1;
+
+public class Galletita {
+	private String forma;
+	private String sabor;
+	
+	public Galletita (String formita, String saborElegido) {
+		forma=formita;
+		sabor=saborElegido;
+	}
+	
+	public String getForma() {
+		return forma;
+	}
+	
+	public String getSabor() {
+		return sabor;
+	}
+}
---
+++ prueba.java
@@ -0,0 +1,12 @@
+package proyecto1TDPparte1;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		Galletita primerPedido= new Galletita("estrella", "vainilla");
+		Galletita segundoPedido= new Galletita("corazon" , "chocolate");
+		
+
+	}
+
+}
